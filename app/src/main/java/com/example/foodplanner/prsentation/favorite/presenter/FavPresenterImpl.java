abce0536021a6428c709ca5b1a8a4d833e79fa8c
shahdashraf2003 <shahdashraf396@gmail.com>
2026-01-30 12:45:23 +0200

    handle error and loading of fav and calender

--- app/src/main/java/com/example/foodplanner/prsentation/favorite/presenter/FavPresenterImpl.java
+++ app/src/main/java/com/example/foodplanner/prsentation/favorite/presenter/FavPresenterImpl.java
@@ -2,40 +2,54 @@ package com.example.foodplanner.prsentation.favorite.presenter;
 
 import android.content.Context;
 
-import androidx.lifecycle.LiveData;
-
 import com.example.foodplanner.data.meal.MealRepo;
 import com.example.foodplanner.data.meal.model.Meal;
 import com.example.foodplanner.prsentation.favorite.view.FavView;
 
-import java.util.List;
-
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Completable;
-import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
-public class FavPresenterImpl  implements FavPresenter {
+public class FavPresenterImpl implements FavPresenter {
 
-        private MealRepo mealRepo;
-        private FavView favView;
-        public FavPresenterImpl(Context context, FavView favView) {
-            this.mealRepo = new MealRepo(context);
-            this.favView=favView;
-        }
+    private MealRepo mealRepo;
+    private FavView favView;
 
-
-        @Override
-        public Single<List<Meal>> getFavMeals() {
-            return  mealRepo.getFavMeals();
-
-        }
-
-    public Completable deleteFavMeal(Meal meal) {
-
-       return mealRepo.deleteFavMeal(meal)
-                .subscribeOn(Schedulers.io());
-
+    public FavPresenterImpl(Context context, FavView favView) {
+        this.mealRepo = new MealRepo(context);
+        this.favView = favView;
     }
-}
 
+    @Override
+    public void getFavMeals() {
+        mealRepo.getFavMeals()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        meals -> {
+                            if (meals == null || meals.isEmpty()) {
+                                favView.onNoMealsFounded();
+                            } else {
+                                favView.showMeals(meals);
+                            }
+                        },
+                        throwable -> favView.showMessage("Failed to load favorites")
+                );
+    }
+
+    @Override
+    public void deleteFavMeal(Meal meal) {
+        mealRepo.deleteFavMeal(meal)
+                .subscribeOn(Schedulers.io())
+                .andThen(mealRepo.getFavMeals())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        meals -> {
+                            favView.showMessage("Meal removed");
+                            favView.showMeals(meals);
+                        },
+                        throwable -> favView.showMessage("Failed to delete meal")
+                );
+    }
+
+
+}
